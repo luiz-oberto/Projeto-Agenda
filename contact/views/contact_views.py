b14fd30ce211324458606d46011fbeaac09512c8
Luiz Oberto <lom.raiol@gmail.com>
2024-09-26 09:46:20 -0300

    aula 467 - Criando o template, a view e a url para exibir contato único

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .order_by('-id')[0:10]
+
+    print(contacts.query)
 
     context = {
         'contacts': contacts,
