24d1b3074c3aeb9157269a9b9a63d185af7ed46b
Luiz Oberto <lom.raiol@gmail.com>
2024-10-15 22:37:25 -0300

    aula 478 - Usando request.method e request.POST para saber quando o formulário é postado

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -5,9 +5,24 @@ from django.shortcuts import get_object_or_404, redirect, render
 from contact.models import Contact
 
 def create(request):
+    # search = request.POST.get('first_name')
+    if request.method == 'POST':
+        print()
+        print(request.method)
+        print(request.POST.get('first_name'))
+        print(request.POST.get('last_name'))
+        print()
+        
+
+
     context = {
 
     }
+
+    print()
+    print(request.method)
+    print()
+
 
     return render(
         request,
