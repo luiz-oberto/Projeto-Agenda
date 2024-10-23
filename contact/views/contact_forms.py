7a1739ed80774d81bac7994835de22a73e5d1f17
Luiz Oberto <lom.raiol@gmail.com>
2024-10-22 22:19:37 -0300

    aula 485 - Verificando form.is_valid, salvando com form.save e redirecionando a página

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from contact.forms import ContactForm
 
 
 def create(request):
     if request.method == 'POST':
+        form = ContactForm(request.POST)
         context = {
-            'form': ContactForm(request.POST)
-    }
+            'form': form
+        }
 
+        if form.is_valid():
+            # commit = False > não salva diretamente no banco de dados
+            # contact = form.save(commit=False)
+            # contact.show = True
+            # contact.save()
+            form.save()
+            return redirect('contact:create')
 
         return render(
             request,
