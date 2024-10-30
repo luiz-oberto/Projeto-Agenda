a987f36fbd5951c3b3494f639022b3c8c8c9fd4c
Luiz Oberto <lom.raiol@gmail.com>
2024-10-29 22:22:00 -0300

    aula 488 - Trabalhando com ImageField no template e nos forms

--- contact/forms.py
+++ contact/forms.py
@@ -3,51 +3,20 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
-    first_name = forms.CharField(
-        widget=forms.TextInput(
+    picture = forms.ImageField(
+        widget=forms.FileInput(
             attrs={
-                'class':'classe-a classe-b',
-                'placeholder': 'Aqui veio do init',
+                'accept': 'image/*',
             }
-        ),
-        label='Primeiro Nome',
-        help_text='Texto de ajuda para seu usuário'
+        )
     )
-    
-    # qualquer = forms.CharField(
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             'class':'classe-a classe-b',
-    #             'placeholder': 'Aqui veio do init',
-    #         }
-    #     ),
-    #     help_text='Texto de ajuda para seu usuário',
-    # )
-
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # atualizando o widget
-        # self.fields['first_name'].widget.attrs.update({
-        #     'class':'classe-a classe-b',
-        #     'placeholder': 'Aqui veio do init',
-        # })
-
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
+            'picture',
         )
-        # Criando um novo widget pro campo
-        # widgets = {
-        #     'first_name':forms.TextInput(
-        #         attrs={
-        #            
-        #         }
-        #     )
-        # }
 
     def clean(self):
         cleaned_data = self.cleaned_data
